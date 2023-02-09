62f216293129aaddfd8b46d2f5433c9d2fb66906
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2023-02-09 09:53:30 -0400

    Fix: Prisma working on Node 18

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as SegfaultHandler from 'segfault-handler';
+
+SegfaultHandler.registerHandler('crash.log');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
