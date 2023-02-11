2723b95f1865ce8648a27496f41a758fcc1a0cfb
Arturo Hernández <arturoaureliohr@gmail.com>
2023-02-11 18:12:12 -0400

    TASK-165: Add Auth0 Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthzModule } from './modules/authz/authz.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AuthzModule
   ],
   controllers: [AppController],
   providers: [AppService],
