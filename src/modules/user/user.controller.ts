812599de6cfe510a6024c9741cc9620521859e5d
Arturo Hernández <arturoaureliohr@gmail.com>
2023-02-11 19:43:39 -0400

    TASK-165: Implement User Controller endpoints to test authentication

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @UseGuards(AuthGuard('jwt')) // Temporal
+  @Get()
+  async test() {
+    return;
+  }
+
+  @Get('/auth')
+  async getAuthToken() {
+    return;
+  }
+}
