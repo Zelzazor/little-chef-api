ba71d57e8725f7b0b55ecaacd0d058b59e042ce6
Arturo Hernández <arturoaureliohr@gmail.com>
2023-02-26 17:02:48 -0400

    TASK-172: Implement search function based on the identification used

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,16 @@
+import { User } from '.prisma/client';
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prismaService: PrismaService) { }
+
+  getUserBySubject = async (subject: string): Promise<User> => {
+    return this.prismaService.user.findUniqueOrThrow({ where: { subject } })
+  }
+
+  getUserById = async (id: string): Promise<User> => {
+    return this.prismaService.user.findUniqueOrThrow({ where: { id } })
+  }
+}
