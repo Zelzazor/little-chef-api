3f2f957bcf5492676f158146402209b82e71344c
Arturo Hernández <arturoaureliohr@gmail.com>
2023-03-18 18:17:43 -0400

    TASK-188: Implement submission status change on downvote or upvote count reaching 3

--- src/modules/vote/vote.service.ts
+++ src/modules/vote/vote.service.ts
@@ -1,19 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { SubmissionStatus } from '@prisma/client';
 import { PrismaService } from '../../prisma/prisma.service';
+
 @Injectable()
 export class VoteService {
   constructor(private prisma: PrismaService) {}
+
   async voteSubmission(
     userId: string,
     submissionId: string,
     isUpvote: boolean,
   ) {
-    const test = await this.prisma.submissionVote.create({
+    const submissionVote = await this.prisma.submissionVote.create({
       data: { userId, submissionId, isUpvote: !!isUpvote },
     });
-    console.log(test);
-    return { msg: 'submission voted' };
+
+    const submissionUpvoteCount = await this.prisma.submissionVote.count({
+      where: { submissionId, isUpvote: true },
+    });
+
+    const submissionDownvoteCount = await this.prisma.submissionVote.count({
+      where: { submissionId, isUpvote: false },
+    });
+
+    if (submissionUpvoteCount >= 3)
+      await this.prisma.submission.update({
+        where: {
+          id: submissionId,
+        },
+        data: {
+          status: SubmissionStatus.APPROVED,
+        },
+      });
+
+    if (submissionDownvoteCount >= 3)
+      await this.prisma.submission.update({
+        where: {
+          id: submissionId,
+        },
+        data: {
+          status: SubmissionStatus.REJECTED,
+        },
+      });
+
+    return submissionVote;
   }
 
   async randomUnvotedSubmission(id: string) {
