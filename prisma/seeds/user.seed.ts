9f6de0e2f88a7894d3c59afa3dffd41beec1202a
Arturo Hernández <arturoaureliohr@gmail.com>
2023-03-18 17:37:07 -0400

    Add test user to seed

--- prisma/seeds/user.seed.ts
+++ prisma/seeds/user.seed.ts
@@ -15,6 +15,18 @@ const user: User[] = [
     updatedAt: new Date(),
     deletedAt: null,
   },
+  {
+    id: 'd486121d-08b1-416d-a1a6-9e2ed0a695d7',
+    name: 'Galcake',
+    nickName: null,
+    email: 'nenas92799@galcake.com',
+    roleId: 'd69c6b14-0849-4f22-917a-e837e695ae05',
+    subject: 'auth0|64162e0d739976b7470d665f',
+    birthDate: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+  },
 ];
 
 export const userSeed: Seed = {
