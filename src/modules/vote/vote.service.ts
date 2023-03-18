ae1daa01207223beef8e95b05dc349aa7fa04fc1
Arturo Hernández <arturoaureliohr@gmail.com>
2023-03-18 17:57:18 -0400

    TASK-188: Fix Submission Status enum import

--- src/modules/vote/vote.service.ts
+++ src/modules/vote/vote.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Status as SubmissionStatus } from '@prisma/client';
+import { SubmissionStatus } from '@prisma/client';
 import { PrismaService } from '../../prisma/prisma.service';
 @Injectable()
 export class VoteService {
