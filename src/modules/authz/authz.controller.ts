bc3ba0fdee274d09503f19e449e5a0424671fb95
Arturo Hernández <arturoaureliohr@gmail.com>
2023-03-24 00:08:49 -0400

    TASK-198: Add security safeguard to prevent usage of endpoint to get token in prod

--- src/modules/authz/authz.controller.ts
+++ src/modules/authz/authz.controller.ts
@@ -1,16 +1,22 @@
 import { Body, Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AuthzService } from './authz.service';
 import { GetTokenRequestDTO } from './dto/get-token.request.dto';
 import { GetTokenResponseDTO } from './dto/get-token.response.dto';
 
 @Controller('auth')
 export class AuthzController {
-  constructor(private authzService: AuthzService) {}
+  constructor(
+    private authzService: AuthzService,
+    private configService: ConfigService,
+  ) {}
 
   @Get('token')
   async getToken(
     @Body() credentials: GetTokenRequestDTO,
   ): Promise<GetTokenResponseDTO> {
+    if ((await this.configService.getOrThrow('NODE_ENV')) !== 'development')
+      throw Error('Unauthorized access attempt to development endpoint');
     return await this.authzService.getToken(credentials);
   }
 }
