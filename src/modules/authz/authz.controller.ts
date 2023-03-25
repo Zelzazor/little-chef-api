2d683075a59535cbe486519c209007ec60307139
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2023-03-24 20:54:21 -0400

    Create Environment Guard to block requests on incorrect env

--- src/modules/authz/authz.controller.ts
+++ src/modules/authz/authz.controller.ts
@@ -1,22 +1,19 @@
 import { Body, Controller, Get } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { AuthzService } from './authz.service';
 import { GetTokenRequestDTO } from './dto/get-token.request.dto';
 import { GetTokenResponseDTO } from './dto/get-token.response.dto';
+import { Environment } from './enums/environment.enum';
+import { EnvironmentIs } from './environment/environment.decorator';
 
 @Controller('auth')
 export class AuthzController {
-  constructor(
-    private authzService: AuthzService,
-    private configService: ConfigService,
-  ) {}
+  constructor(private authzService: AuthzService) {}
 
   @Get('token')
+  @EnvironmentIs(Environment.Development)
   async getToken(
     @Body() credentials: GetTokenRequestDTO,
   ): Promise<GetTokenResponseDTO> {
-    if ((await this.configService.getOrThrow('NODE_ENV')) !== 'development')
-      throw Error('Unauthorized access attempt to development endpoint');
     return await this.authzService.getToken(credentials);
   }
 }
