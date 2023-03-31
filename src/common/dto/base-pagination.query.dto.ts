ba3eae71b28b87842b528052aa1453d54154cf9b
Arturo Hernández <arturoaureliohr@gmail.com>
2023-03-31 00:05:04 -0400

    TASK-210: Repurpose Base Pagination Request DTO to be a Query DTO

--- src/common/dto/base-pagination.query.dto.ts
+++ src/common/dto/base-pagination.query.dto.ts
@@ -1,11 +1,14 @@
+import { Type } from 'class-transformer';
 import { IsNumber, IsOptional } from 'class-validator';
 
-export class BasePaginationRequestDto {
+export class BasePaginationQueryDto {
   @IsOptional()
   @IsNumber()
+  @Type(() => Number)
   page?: number;
 
   @IsOptional()
   @IsNumber()
+  @Type(() => Number)
   pageSize?: number;
 }
