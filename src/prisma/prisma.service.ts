5e488f53a96cd20fb1144bb051c53b35e38051b4
Arturo Hernández <arturoaureliohr@gmail.com>
2023-03-30 23:54:24 -0400

    TASK-210: Implement findManyPaginated function in Prisma Service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,7 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { BasePaginationRequestDto } from '../common/dto/base-pagination.query.dto';
+import { PaginatedQueryResponseDto } from '../common/dto/paginated-query.response.dto';
 import { applyMiddleware } from './softDelete.middleware';
 
 @Injectable()
@@ -17,5 +19,36 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     this.$on('beforeExit', async () => {
       await app.close();
     });
+  }
+
+  async findManyPaginated<T>(
+    modelName: string,
+    params: any,
+    paginationOptions: BasePaginationRequestDto,
+  ): Promise<PaginatedQueryResponseDto<T>> {
+    const page = paginationOptions?.page || 1;
+    const pageSize = paginationOptions?.pageSize || 10;
+
+    const extendedParams = {
+      ...params,
+      skip: (page - 1) * pageSize,
+      take: pageSize,
+    };
+
+    const model = this[modelName as keyof this] as any;
+
+    const result = await model.findMany(extendedParams);
+
+    const count = await model.count(params);
+
+    return {
+      data: result,
+      pagination: {
+        page,
+        pageSize,
+        totalItems: count,
+        totalPages: Math.ceil(count / pageSize),
+      },
+    };
   }
 }
