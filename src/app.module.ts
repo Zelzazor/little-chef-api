5fe5107034a1f719f1e7ce27b9fcf7994e9e71e0
Arturo Hernández <arturoaureliohr@gmail.com>
2023-04-29 09:28:34 -0400

    TASK-232: Add Ingredient module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthzModule } from './modules/authz/authz.module';
+import { IngredientModule } from './modules/ingredient/ingredient.module';
 import { RecipesModule } from './modules/recipes/recipes.module';
 import { S3UploadModule } from './modules/s3-upload/s3-upload.module';
 import { SubmissionModule } from './modules/submission/submission.module';
@@ -20,6 +21,7 @@ import { PrismaModule } from './prisma/prisma.module';
     UserModule,
     RecipesModule,
     SubmissionModule,
+    IngredientModule,
     VoteModule,
     S3UploadModule,
   ],
