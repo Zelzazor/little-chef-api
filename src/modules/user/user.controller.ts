7c993d2786e3c4c9427750b716e59fe321b7aea3
Daniel Rondón <danielrondon00@gmail.com>
2023-06-17 17:58:14 -0400

    TASK-221: add endpoint to get all user

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,11 +3,14 @@ import {
   Controller,
   Get,
   Post,
+  Query,
   RawBodyRequest,
   Req,
 } from '@nestjs/common';
 import { Request } from 'express';
+import { BasePaginationQueryDto } from 'src/common/dto/base-pagination.query.dto';
 import { Auth } from '../authz/auth.decorator';
+import { Role } from '../authz/enums/role.enum';
 import { UpdateUserRequestDto } from './dto/update-user.request.dto';
 import { UpdateUserResponseDto } from './dto/update-user.response.dto';
 import { UserService } from './user.service';
@@ -36,5 +39,11 @@ export class UserController {
     };
 
     return await this.userService.updateUser(updatedUser);
+  }
+  @Get('all')
+  @Auth(Role.Admin)
+  getAllUsers(@Query() { page, pageSize }: BasePaginationQueryDto) {
+    console.log(page, pageSize);
+    return this.userService.getAllUsers({ page, pageSize });
   }
 }
